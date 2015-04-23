516913c064be29735b49a85b3ad009447dddbbda
hernan <hboykier@gmail.com>
2015-04-22 23:52:49 -0300

    Nuevo

---
+++ database/migrations/2015_04_23_015729_create_domains.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDomains extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('domains', function(Blueprint $table)
+		{
+            $table->increments('id');
+            $table->string('code', 20)->unique();
+            $table->string('name', 100)->unique();
+            $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('domains');
+	}
+
+}
