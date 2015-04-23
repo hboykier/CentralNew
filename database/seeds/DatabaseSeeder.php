8d3d634a8abc2915863e3bbff616af11fdc81f5b
hernan <hboykier@gmail.com>
2015-04-22 23:40:12 -0300

    Nuevo

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder {
 
@@ -12,9 +12,8 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		Model::unguard();
-
-		// $this->call('UserTableSeeder');
+        Model::unguard();
+		$this->call('DomainSeeder');
 	}
 
 }
